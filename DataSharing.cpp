769defe3007cd24d0ad6a5a2f5ae44fe13f556a9
alpineQ <mk_dev@mail.ru>
2021-11-27 23:25:47 +0300

    Рабочее распределение данных между MPI-процессами

--- DataSharing.cpp
+++ DataSharing.cpp
@@ -3,7 +3,6 @@
 #include "InputData.h"
 
 #define ROOT_RANK 0
-#define DATA_SEND_TAG 0
 
 
 void sendTypes(int nProcesses, int* dataPerProcess, InputData input) {
@@ -11,7 +10,7 @@ void sendTypes(int nProcesses, int* dataPerProcess, InputData input) {
     int *displacements = new int[nProcesses];
     for (unsigned i = 0; i < nProcesses; ++i) {
         types[i] = input.sequences[i].type;
-        displacements[i] = (i == 0) ? 0 : displacements[i - 1] + dataPerProcess[i];
+        displacements[i] = 0;
     }
     MPI_Scatterv(types,
                  dataPerProcess,
@@ -40,35 +39,39 @@ bool* recvTypes(int nSequences) {
     return types;
 }
 
-void sendData(int nProcesses, const int* dataPerProcess, InputData input) {
-    for (int process = 0, currentSequence = 0; process < nProcesses; ++process) {
-        if (process == ROOT_RANK)
-            continue;
+vector<Sequence> sendData(const int* dataPerProcess, InputData input) {
+    vector<Sequence> rootData;
+    for (int process = 0, sequence = 0; sequence < input.nSequences; ++process) {
         MPI_Request request;
-        for (int j = 0; j < dataPerProcess[process]; ++j) {
-            MPI_Isend(input.sequences[currentSequence].data,
-                      input.sequenceLength,
-                      (input.sequences[currentSequence].type) ? MPI_INT : MPI_FLOAT,
-                      process,
-                      DATA_SEND_TAG,
-                      MPI_COMM_WORLD,
-                      &request);
-            ++currentSequence;
+        for (int i = 0; i < dataPerProcess[process]; ++i, ++sequence) {
+            if (process == ROOT_RANK)
+                rootData.push_back(input.sequences[sequence]);
+            else
+                MPI_Isend(input.sequences[sequence].data,
+                          input.sequenceLength,
+                          (input.sequences[sequence].type) ? MPI_INT : MPI_FLOAT,
+                          process,
+                          i,
+                          MPI_COMM_WORLD,
+                          &request);
         }
     }
+    return rootData;
 }
 
 vector<Sequence> recvData(int nSequences, int sequenceLength, const bool* types) {
     vector<Sequence> sequences;
     for (int i = 0; i < nSequences; ++i) {
+        Sequence sequence(sequenceLength, types[i]);
         MPI_Status status;
-        MPI_Recv(sequences[i].data,
+        MPI_Recv(sequence.data,
                  sequenceLength,
                  (types[i]) ? MPI_INT : MPI_FLOAT,
                  ROOT_RANK,
-                 DATA_SEND_TAG,
+                 i,
                  MPI_COMM_WORLD,
                  &status);
+        sequences.push_back(sequence);
     }
     return sequences;
 }
@@ -98,7 +101,6 @@ vector<Sequence> getDataFromRoot() {
 
 vector<Sequence> sendDataToProcesses(InputData input, int nProcesses) {
     int *dataPerProcess = input.getDataPerProcess(nProcesses);
-
     MPI_Bcast(&input.sequenceLength, 1, MPI_INT, ROOT_RANK, MPI_COMM_WORLD);
     MPI_Scatter(dataPerProcess,
                 1,
@@ -109,10 +111,8 @@ vector<Sequence> sendDataToProcesses(InputData input, int nProcesses) {
                 ROOT_RANK,
                 MPI_COMM_WORLD);
     sendTypes(nProcesses, dataPerProcess, input);
-    sendData(nProcesses, dataPerProcess, input);
-    vector<Sequence> sequences;
-    for (unsigned i = 0; i < dataPerProcess[ROOT_RANK]; ++i)
-        sequences.push_back(sequences[i * nProcesses]);
+    vector<Sequence> sequences = sendData(dataPerProcess, input);
+
     delete[] dataPerProcess;
     return sequences;
 }
